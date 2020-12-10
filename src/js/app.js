0991485c0a8eb1e31524712d2edfc38d72f7b9a6
Nikita Rastorguev <nirastor@gmail.com>
2020-12-10 12:46:13 +0300

    add export to JSON

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import FormColorpicker from './formColorpicker';
 import SelectedColor from './selectedColor';
 import BannerConfig from './bannerConfig';
@@ -9,20 +10,26 @@ export default class App {
     this.bannerConfig = new BannerConfig();
     this.banner = new Banner(this.bannerEl, this.bannerConfig);
 
-    this.gradientOpenBtn = document.querySelector('.gradient-open-btn');
+    this.formControls = document.querySelector('.form-controls');
 
-    this.mainColorSelector = document.querySelector('.color--main');
+    this.gradientOpenBtn = this.formControls.querySelector('.gradient-open-btn');
+
+    this.mainColorSelector = this.formControls.querySelector('.color--main');
     this.mainColor = new SelectedColor(this.mainColorSelector, 'main');
 
-    this.gradientSelector = document.querySelector('.color--gradient');
+    this.gradientSelector = this.formControls.querySelector('.color--gradient');
     this.gradientColor = new SelectedColor(this.gradientSelector, 'gradient');
+
+    this.textarea = this.formControls.querySelector('.textarea');
+    this.linkInputEl = this.formControls.querySelector('.input-link');
+    this.imageInputEl = this.formControls.querySelector('.input-image');
+
+    this.buttonPng = this.formControls.querySelector('.js-btn-png');
+    this.buttonHtml = this.formControls.querySelector('.js-btn-html');
+    this.buttonJson = this.formControls.querySelector('.js-btn-json');
 
     this.formColorEl = document.querySelector('.form-color');
     this.formColorpicker = new FormColorpicker(this.formColorEl);
-
-    this.textarea = document.querySelector('.textarea');
-    this.linkInputEl = document.querySelector('.input-link');
-    this.imageInputEl = document.querySelector('.input-image');
 
     this.state = {
       gradientSelectorIsOpen: false,
@@ -39,6 +46,12 @@ export default class App {
       main: this.mainColor,
       gradient: this.gradientColor,
     };
+    this.saveButtonsText = {
+      copied: 'Скопированно',
+      pngBtn: 'Загрузить PNG',
+      htmlBtn: 'Копировать HTML',
+      jsonBtn: 'Копировать настройки',
+    };
   }
 
   init() {
@@ -47,6 +60,10 @@ export default class App {
   }
 
   initListeners() {
+    this.formControls.addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+
     this.formColorpicker.initListeners(
       this.closeColorpicker.bind(this),
       this.submitColorpicker.bind(this),
@@ -85,6 +102,21 @@ export default class App {
     this.imageInputEl.addEventListener('focus', (e) => {
       e.preventDefault();
       this.onImageInputFocus();
+    });
+
+    this.buttonPng.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.exportPng();
+    });
+
+    this.buttonHtml.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.exportHtml();
+    });
+
+    this.buttonJson.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.exportJson();
     });
   }
 
@@ -153,6 +185,47 @@ export default class App {
     if (this.imageInputEl.value) {
       this.banner.redrawImage();
     }
+  }
+
+  exportPng() {
+    console.log('export png');
+    console.log(this.state.colorpicker.isOpen);
+  }
+
+  showSuccessCopy(btn) {
+    const bText = this.saveButtonsText;
+    if (btn.innerText !== bText.copied) {
+      // eslint-disable-next-line no-param-reassign
+      btn.innerText = bText.copied;
+      setTimeout(() => {
+        // eslint-disable-next-line no-param-reassign
+        btn.innerText = bText[btn.dataset.textId];
+      }, 1500);
+    }
+  }
+
+  exportJson() {
+    console.log('export json');
+    const text = JSON.stringify(this.bannerConfig);
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        this.showSuccessCopy(this.buttonJson);
+      })
+      .catch((err) => {
+        console.log('Something went wrong', err);
+      });
+  }
+
+  exportHtml() {
+    console.log('export html');
+    const text = JSON.stringify(this.bannerConfig);
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        this.showSuccessCopy(this.buttonHtml);
+      })
+      .catch((err) => {
+        console.log('Something went wrong', err);
+      });
   }
 }
 
